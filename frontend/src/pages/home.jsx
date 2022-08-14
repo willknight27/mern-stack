b43427e6afea3f4acc9049805ad78c82bb2e3512
Williams Caballero Araya <williams.caballeroa@gmail.com>
2022-08-14 13:40:43 -0400

    Get all games data with axios

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react"
+import axios from 'axios';
+// components
+import GameDetails from "../components/GameDetails"
+
 const Home = () => {
+    const [games, setGames] = useState(null)
+
+    useEffect(() => {
+        
+        axios.get('http://localhost:4000/api/games')
+            .then(res =>{
+                const games = res.data
+                console.log(games);
+                setGames(games)
+            })
+        
+
+    }, [])
+
     return (
         <div className="home">
-            <h2>Home</h2>
+            <div className="workouts">
+                {games && games.map(game => (
+                    <GameDetails game={game} key={game._id} />
+                ))}
+            </div>
         </div>
     )
 }
