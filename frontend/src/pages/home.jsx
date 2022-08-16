0e043711280e10b0f31a683efe4883b6402c9f90
Williams Caballero Araya <williams.caballeroa@gmail.com>
2022-08-16 16:40:44 -0400

    Add game form

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import axios from 'axios';
 // components
 import GameDetails from "../components/GameDetails"
+import GameForm from "../components/GameForm";
 
 const Home = () => {
     const [games, setGames] = useState(null)
@@ -20,12 +21,15 @@ const Home = () => {
 
     return (
         <div className="home">
-            <div className="workouts">
+            <div className="games">
                 {games && games.map(game => (
                     <GameDetails game={game} key={game._id} />
                 ))}
             </div>
+            <GameForm/>
         </div>
+
+
     )
 }
 
